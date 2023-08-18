795d511f8d06b69ddd8fef74da47295a91da518e
robert-ganescu_off5th <robert.ganescu@saksoff5th.com>
2023-08-18 15:54:26 +0300

    EBE-10: Update user schemas

--- app/database/schemas/user.py
+++ app/database/schemas/user.py
@@ -1,52 +1,102 @@
-from pydantic import BaseModel, Field, EmailStr, SecretStr, validator
+from pydantic import ConfigDict, BaseModel, EmailStr, SecretStr, field_validator
+
+
+def get_properties_password_description():
+    password_requirements = [
+        "between 8 and 16 characters long",
+        "contain at least one digit",
+        "one uppercase letter",
+        "one symbol.",
+    ]
+
+    return "Password must be" + ", ".join(password_requirements)
 
 
 class UserRegister(BaseModel):
-    email: EmailStr = Field(
-        ...,
-        title="Email Address",
-        description="Valid email address for user registration.",
-        example="user@example.com",
-    )
-    password: SecretStr = Field(
-        ...,
-        title="Password",
-        description="Password must be between 8 and 16 characters long, and contain at least one digit, one letter, and one symbol (e.g., !@#$%). This field is sensitive and should be kept secret.",
-        example="StrongPass123!",
-    )
-    is_superuser: bool = Field(
-        False,
-        title="Superuser Status",
-        description="Set to True if the user is a superuser, otherwise False.",
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "email": "user@example.com",
+                "password": "StrongPass123!",
+                "is_superuser": False,
+            },
+            "properties": {
+                "email": {
+                    "type": "string",
+                    "format": "email",
+                    "description": "Valid email address for user registration.",
+                },
+                "password": {
+                    "type": "string",
+                    "format": "password",
+                    "minLength": 8,
+                    "maxLength": 16,
+                    "description": get_properties_password_description(),
+                },
+                "is_superuser": {
+                    "type": "boolean",
+                    "description": "True if the user is a superuser, otherwise False.",
+                },
+            },
+        },
+        validate_default=True,
     )
 
-    @validator("password")
+    email: EmailStr
+    password: SecretStr
+    is_superuser: bool
+
+    @field_validator("password")
     def validate_password(cls, value):
         # Check for the size of password
         if not (8 <= len(value) <= 16):
             raise ValueError("Password length must be between 8 and 16 characters.")
 
         # Check for at least one uppercase letter
-        if not any(c.isupper() for c in value.get_secret_value()):
+        if not any(c.isupper() for c in value):
             raise ValueError("Password must contain at least one uppercase letter")
 
         # Check for at least one lowercase letter
-        if not any(c.islower() for c in value.get_secret_value()):
+        if not any(c.islower() for c in value):
             raise ValueError("Password must contain at least one lowercase letter")
 
         # Check for at least one digit
-        if not any(c.isdigit() for c in value.get_secret_value()):
+        if not any(c.isdigit() for c in value):
             raise ValueError("Password must contain at least one digit")
 
         # Check for at least one symbol
-        if not any(
-            c in "!@#$%^&*()-_=+[]{};:'\",.<>/?\\|" for c in value.get_secret_value()
-        ):
+        if not any(c in "!@#$%^&*()-_=+[]{};:'\",.<>/?\\|" for c in value):
             raise ValueError("Password must contain at least one symbol")
 
         return value
 
 
 class UserLogin(BaseModel):
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "email": "user@example.com",
+                "password": "StrongPass123!",
+            },
+            "properties": {
+                "email": {
+                    "type": "string",
+                    "format": "email",
+                    "title": "Email Address",
+                    "description": "Valid email address for user login.",
+                    "example": "user@example.com",
+                },
+                "password": {
+                    "type": "string",
+                    "format": "password",
+                    "title": "Password",
+                    "description": "User password for login.",
+                    "example": "StrongPass123!",
+                },
+            },
+        },
+        validate_default=True,
+    )
+
     email: EmailStr
     password: SecretStr
