ff36752032195203f32e4fbe4342cfc421701c10
robert-ganescu_off5th <robert.ganescu@saksoff5th.com>
2023-08-18 14:44:12 +0300

    EBE-10: Include user router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.routers import root
+from app.api.routers import root, user
 
 app = FastAPI()
 
 app.include_router(root.router)
+app.include_router(user.router)
